3724624cb13860635c0818f85b85ca288db9fa94
Sergey Kuryanov <sergey.kuryanov@gmail.com>
2016-10-20 21:46:31 +0300

    Tests added

---
+++ Demo/librato-iOS DemoTests/NSString+SanitizedForMetricTests.m
@@ -0,0 +1,36 @@
+//
+//  NSString+SanitizedForMetricTests.m
+//  librato-iOS
+//
+//  Created by Sergey Kuryanov on 20.10.16.
+//  Copyright © 2016 Amco International Education Services, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "NSString+SanitizedForMetric.h"
+
+@interface NSString_SanitizedForMetricTests : XCTestCase
+@property (nonatomic, strong) NSString *testString;
+@end
+
+@implementation NSString_SanitizedForMetricTests
+
+- (void)setUp {
+    [super setUp];
+    
+    self.testString = @"A-Za-z0-9.:-_<>{}[];\'\"!@#$%^&*()_+=œ∑´®†¥¨ˆøπ“‘«åß∂ƒ©˙∆˚¬…æΩ≈ç√∫˜µ≤≥÷";
+}
+
+- (void)tearDown {
+    self.testString = nil;
+    
+    [super tearDown];
+}
+
+- (void)testThatOnlyAllowedCharactersPresent {
+    NSString *expectedString = @"A-Za-z0-9.:-_-------------------_------------------------------------";
+    
+    XCTAssertEqualObjects(self.testString.sanitizedForMetric, expectedString);
+}
+
+@end
